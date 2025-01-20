5528cc292460a56d9a4681fa8546de2bf2009f54
redowansami <bsse1409@iit.du.ac.bd>
2025-01-20 15:52:04 +0600

    refactored

---
+++ frontend/login_app/lib/pages/space_owner_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SpaceOwnerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Space Owner Dashboard')),
+      body: Center(child: Text('Welcome, Space Owner!')),
+    );
+  }
+}
